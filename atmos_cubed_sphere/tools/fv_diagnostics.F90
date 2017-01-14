39b21e3e1a4a37632baf174436da66a0124b960e
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2017-01-13 20:28:11 -0500

    Fix a bug in fv_diagnostics.F90 to prevent using delz when hydrostatic mode is on.

--- atmos_cubed_sphere/tools/fv_diagnostics.F90
+++ atmos_cubed_sphere/tools/fv_diagnostics.F90
@@ -1264,7 +1264,7 @@ contains
     real, parameter:: ws_1 = 20.
     real, parameter:: vort_c0= 2.2e-5 
     logical, allocatable :: storm(:,:), cat_crt(:,:)
-    real :: tmp2, pvsum, e2, einf, qm, mm, maxdbz, allmax
+    real :: tmp2, pvsum, e2, einf, qm, mm, maxdbz, allmax, rgrav
     integer :: Cl, Cl2
 
     !!! CLEANUP: does it really make sense to have this routine loop over Atm% anymore? We assume n=1 below anyway
@@ -2533,6 +2533,21 @@ contains
 
        if ( idiag%id_u100m>0 .or. idiag%id_v100m>0 .or. idiag%id_w100m>0 .or. idiag%id_w5km>0 .or. idiag%id_w2500m>0 .or. idiag%id_basedbz .or. idiag%id_dbz4km) then
           if (.not.allocated(wz)) allocate ( wz(isc:iec,jsc:jec,npz+1) )
+          if ( Atm(n)%flagstruct%hydrostatic) then
+             rgrav = 1. / grav
+!$OMP parallel do default(none) shared(isc,iec,jsc,jec,wz,npz,Atm,n,rgrav)
+            do j=jsc,jec
+               do i=isc,iec
+                  wz(i,j,npz+1) = 0.
+!                  wz(i,j,npz+1) = Atm(n)%phis(i,j)/grav
+               enddo
+               do k=npz,1,-1
+                  do i=isc,iec
+                     wz(i,j,k) = wz(i,j,k+1) - (rdgas*rgrav)*Atm(n)%pt(i,j,k)*(Atm(n)%peln(i,k,j) - Atm(n)%peln(i,k+1,j))
+                  enddo
+               enddo
+            enddo
+          else
 !$OMP parallel do default(none) shared(isc,iec,jsc,jec,wz,npz,Atm,n)
             do j=jsc,jec
                do i=isc,iec
@@ -2545,6 +2560,7 @@ contains
                   enddo
                enddo
             enddo
+         endif
             if( prt_minmax )   &
             call prt_maxmin('ZTOP', wz(isc:iec,jsc:jec,1)+Atm(n)%phis(isc:iec,jsc:jec)/grav, isc, iec, jsc, jec, 0, 1, 1.E-3)
        endif

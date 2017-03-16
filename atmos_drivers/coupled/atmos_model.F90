aae7df332a50b75099f785df9d2acdf794b43216
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2017-03-16 12:57:39 -0400

    Fix a potential issue when fhzero is less than 1.

--- atmos_drivers/coupled/atmos_model.F90
+++ atmos_drivers/coupled/atmos_model.F90
@@ -473,12 +473,12 @@ subroutine update_atmos_model_state (Atmos)
     Atmos % Time = Atmos % Time + Atmos % Time_step
 
     call get_time (Atmos%Time - diag_time, isec)
-    if (mod(isec,(3600*nint(IPD_Control%fhzero))) == 0) then
+    if (mod(isec,nint(3600*IPD_Control%fhzero)) == 0) then
       time_int = real(isec)
       if (mpp_pe() == mpp_root_pe()) write(6,*) ' gfs diags time since last bucket empty: ',time_int/3600.,'hrs'
       call gfdl_diag_output(Atmos%Time, Atm_block, IPD_Control%nx, IPD_Control%ny, &
                             IPD_Control%levs, 1, 1, 1.d0, time_int)
-      if (mod(isec,3600*nint(IPD_Control%fhzero)) == 0) diag_time = Atmos%Time
+      if (mod(isec,nint(3600*IPD_Control%fhzero)) == 0) diag_time = Atmos%Time
     endif
     call diag_send_complete_extra (Atmos%Time)
 

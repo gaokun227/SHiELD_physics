419400eba6111a42bf4124255692b80b62087758
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2022-09-13 15:18:10 -0400

    Update inconsistent variables for NOAH-MP.

--- GFS_layer/GFS_physics_driver.F90
+++ GFS_layer/GFS_physics_driver.F90
@@ -1287,7 +1287,7 @@ module module_physics_driver
 !  ---  inputs:
            (im, Model%lsoil, kdt, Statein%pgr,  Statein%ugrs, Statein%vgrs,   &
             Statein%tgrs,  Statein%qgrs, soiltyp, vegtype, sigmaf,     &
-            Radtend%semis, gabsbdlw, adjsfcdsw, adjsfcnsw, dtf,        &
+            Radtend%semis, adjsfcdlw, adjsfcdsw, adjsfcnsw, dtf,       &
             Sfcprop%tg3, cd, cdq, Statein%prsl(:,1), work3,            &
             Diag%zlvl, dry,   wind, slopetyp,                          &
             Sfcprop%shdmin,   Sfcprop%shdmax,  Sfcprop%snoalb,         &
@@ -1315,9 +1315,10 @@ module module_physics_driver
             Sfcprop%fastcpxy, Sfcprop%xlaixy,  Sfcprop%xsaixy,                  &
             Sfcprop%taussxy,  Sfcprop%smoiseq, Sfcprop%smcwtdxy,                &
             Sfcprop%deeprechxy, Sfcprop%rechxy,                                 &
+            Sfcprop%albdvis, Sfcprop%albdnir,  Sfcprop%albivis,  Sfcprop%albinir,&
 !  ---  outputs:
             Sfcprop%sncovr, qss, gflx, drain, evap, hflx, ep1d, runof,          &
-            Diag%cmm, Diag%chh, evbs, evcw, sbsno, snowc, Diag%soilm,  &
+            Diag%cmm, Diag%chh, evbs, evcw, sbsno, snowc, Diag%soilm,           &
             snohf, Diag%smcwlt2, Diag%smcref2, Diag%wet1, t2mmp, q2mp)
 
         endif

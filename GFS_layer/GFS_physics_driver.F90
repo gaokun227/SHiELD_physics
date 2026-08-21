ada6eef0a4c543be237cb2fbfe72718f92ad36ae
Kun Gao <Kun.Gao@noaa.gov>
2026-08-21 13:43:10 -0400

    fix tprcp

--- GFS_layer/GFS_physics_driver.F90
+++ GFS_layer/GFS_physics_driver.F90
@@ -1083,7 +1083,7 @@ module module_physics_driver
             Sfcprop%shdmax(i) = statein%shdmax(i)
             !Sfcprop%srflag(i) = statein%srflag(i)
             Sfcprop%weasd(i) = statein%weasd(i)
-            Sfcprop%tprcp(i) = statein%tprcp(i)
+            !Sfcprop%tprcp(i) = statein%tprcp(i) !KGao: tprcp is not updated in inline phys
             Diag%cmm(i) = statein%cmm(i)
             Diag%chh(i) = statein%chh(i)
          enddo
